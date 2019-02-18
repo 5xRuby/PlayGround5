8b4d2b79f0d4d1cb5b813bf5586cc7af247e35cf
蒼時弦也 <elct9620@frost.tw>
2019-02-18 18:38:23 +0800

    Fix whenever crontab command not generate correct for production

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-set :job_template, "bash -l -c 'export PATH=/usr/local/ruby25/bin:$PATH && :job'" if @environment == 'production'
+if @environment == 'production'
+  set :job_template, "bash -l -c 'export PATH=/usr/local/ruby25/bin:$PATH && :job'"
+  job_type :runner,  'cd :path && bin/rails runner -e :environment ":task" :output'
+end
 
 # TODO: Ensure timezone is correct
 every 1.day at: '0:00 am' do
