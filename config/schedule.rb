ae0d243f9b18df24373885580e6a931ce8ec7431
蒼時弦也 <elct9620@frost.tw>
2019-02-18 18:22:35 +0800

    Use bash -l -c to execute whenever command

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-set :job_template, 'PATH=/usr/local/ruby25/bin:$PATH && :job' if @environment == 'production'
+set :job_template, "bash -l -c 'export PATH=/usr/local/ruby25/bin:$PATH && :job'" if @environment == 'production'
 
 # TODO: Ensure timezone is correct
 every 1.day at: '0:00 am' do
