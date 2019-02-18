70c00a6e43f7c4bb51271429946875174d7c1f42
蒼時弦也 <elct9620@frost.tw>
2019-02-18 18:17:23 +0800

    Configure ruby path for production

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+set :job_template, 'PATH=/usr/local/ruby25/bin:$PATH && :job' if @environment == 'production'
+
 # TODO: Ensure timezone is correct
 every 1.day at: '0:00 am' do
   runner 'Proxmox::VM.all.map(&:reinitialize!)'
