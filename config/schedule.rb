d601314d1dd33c0adf5ceeb23a17a45bb9c47198
蒼時弦也 <elct9620@frost.tw>
2019-10-28 09:57:47 +0800

    Fix cronjob not update to ruby 2.6

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@
 
 if @environment == 'production'
   # TODO: Read Capistrano configure to prevent hardcode
-  set :job_template, "bash -l -c 'export PATH=/usr/local/ruby25/bin:/usr/local/bin:$PATH && :job'"
+  set :job_template, "bash -l -c 'export PATH=/usr/local/ruby26/bin:/usr/local/bin:$PATH && :job'"
   job_type :runner,  'cd :path && bin/rails runner -e :environment ":task" :output'
 end
 
